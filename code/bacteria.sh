9139819c100833e029cf2f3b7f47d01eed52d1d5
allanfari <allan.farinango@est.ikiam.edu.ec>
2022-11-23 20:02:19 -0500

    farinango allan ah contado las bacterias

--- code/bacteria.sh
+++ code/bacteria.sh
@@ -1,1 +1,5 @@
-....1
+cut -f 1 ../data/grampa.csv | sort | wc -l
+cat ../data/grampa.csv | cut -d "." -f 2 | cut -d "," -f 1 | sort | uniq -c | sort | tail -n 1
+cat ../data/grampa.csv | cut -d "." -f 2 | cut -d "," -f 1 | sort | uniq -c | sort | head -n 3 | tail -n 1
+
+
